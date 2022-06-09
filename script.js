be5ada95814fae87e0fea5b9a67e98bed1379457
Jobsidney <jobsidney67@gmail.com>
2022-06-10 00:36:44 +0300

    Calculator js

---
+++ script.js
@@ -0,0 +1,44 @@
+function doCalculator(operator) {
+    var n = document.calcForm.operator.length;
+
+    var flag = false;
+    var i;
+    for (i = 0; i < n; i++) {
+        if (document.calcForm.operator[i].checked) {
+            flag = true;
+            break;
+        }
+    }
+    if (flag == false) {
+        alert("Chose an operator");
+        return false;
+    }
+
+    operator = document.calcForm.operator[i].value;
+
+    var p = parseInt(document.calcForm.num1.value);
+    var q = parseInt(document.calcForm.num2.value);
+
+    var result;
+
+    switch (operator) {
+        case '+':
+            result = p + q;
+            break;
+        case '-':
+            result = p - q;
+            break;
+        case '*':
+            result = p * q;
+            break;
+        case '/':
+            result = p / q;
+            break;
+        default:
+    }
+
+    document.getElementById('results').innerHTML = result;
+
+
+
+}
